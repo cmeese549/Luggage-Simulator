5a6dd7a91af942efb28df22ed4914df1ee33d967
cmeese549 <chris@meesewebdesigns.com>
2025-04-07 13:52:53 -0600

    twinkle

--- Objects/InventoryItems/InventoryItem.gd
+++ Objects/InventoryItems/InventoryItem.gd
@@ -6,10 +6,14 @@ class_name InventoryItem
 @export var item_description : String = "A very cool and epic item.  Maybe you can trade it for a bucket?"
 @export var item_icon : Texture2D = preload("res://icon.svg")
 
+@export var twinkle_diameter_meters : float = 0.5
+
 @onready var sprite : Sprite3D = $Bouncer/Sprite3D
 @onready var text : Label3D = $Bouncer/Label3D
+@onready var twinkle : AudioStreamPlayer3D = $Twinkle
 
 func _ready() -> void:
 	print(self.global_position)
 	sprite.texture = item_icon
 	text.text = item_name
+	twinkle.unit_size = twinkle_diameter_meters
